a625e183272d3c2c42d34a9dd649fd000662ecd0
ColinVenida <colin.venida@gmail.com>
2019-10-23 08:46:22 -0700

    attempt to customize unity dropdown behaviors

---
+++ Scale Generator/Assets/_Scripts/TestScripts/AwesomeDropdown.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AwesomeDropdown : MonoBehaviour
+{
+    public Image dropImage;
+
+    private bool clicked = false;
+    public void ChangeDropColor()
+    {
+        Debug.Log( "changing color" );
+        dropImage.color = new Color( 0.4f, 0.4f, 0.7f );
+    }
+
+    public void ResetColor()    
+    {
+        Debug.Log( "reset color" );
+        dropImage.color = new Color( 1.0f, 1.0f, 1.0f );
+    }
+
+    public void Clicked()
+    {
+        Debug.Log( "clicked = " + clicked );
+        if (!clicked)
+        {
+            dropImage.color = new Color( 0.4f, 0.4f, 0.7f );
+        }
+        else
+        {
+            dropImage.color = new Color( 1.0f, 1.0f, 1.0f );
+        }
+        clicked = !clicked;
+    }
+   
+}
