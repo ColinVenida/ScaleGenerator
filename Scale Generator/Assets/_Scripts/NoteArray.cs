c4eacb64fdea34daf4523ada21b5217a4b1a01f3
ColinVenida <colin.venida@gmail.com>
2023-02-01 16:57:16 -0800

    added Enum for wholetones and semitones

--- Scale Generator/Assets/_Scripts/NoteArray.cs
+++ Scale Generator/Assets/_Scripts/NoteArray.cs
@@ -10,14 +10,17 @@ public class NoteArray : MonoBehaviour
 
     private void Awake ()
     {
+        int whole = (int) ToneType.WHOLE_TONE;
+        int semi = ( int )ToneType.SEMI_TONE;
+
         //populate the noteArray 
-        noteArray[0] = new Note( "A", 0, 2, 2 );
-        noteArray[1] = new Note( "B", 0, 1, 2 );
-        noteArray[2] = new Note( "C", 0, 2, 1 );
-        noteArray[3] = new Note( "D", 0, 2, 2 );
-        noteArray[4] = new Note( "E", 0, 1, 2 );
-        noteArray[5] = new Note( "F", 0, 2, 1 );
-        noteArray[6] = new Note( "G", 0, 2, 2 );
+        noteArray[0] = new Note( "A", 0, whole, whole );
+        noteArray[1] = new Note( "B", 0, semi, whole );
+        noteArray[2] = new Note( "C", 0, whole, semi );
+        noteArray[3] = new Note( "D", 0, whole, whole );
+        noteArray[4] = new Note( "E", 0, semi, whole );
+        noteArray[5] = new Note( "F", 0, whole, semi );
+        noteArray[6] = new Note( "G", 0, whole, whole );
 
     }
 }
---
+++ Scale Generator/Assets/_Scripts/ToneTypes.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum ToneType
+{
+    WHOLE_TONE = 2,
+    SEMI_TONE = 1,
+}
