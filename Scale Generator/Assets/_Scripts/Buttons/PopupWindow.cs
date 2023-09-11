d43258bec55f6f91cd596ff5c28c527c78328ebd
ColinVenida <colin.venida@gmail.com>
2023-09-11 06:57:13 -0700

    PopupWindows change their button's color when active

--- Scale Generator/Assets/_Scripts/Buttons/PopupWindow.cs
+++ Scale Generator/Assets/_Scripts/Buttons/PopupWindow.cs
@@ -1,17 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class PopupWindow : MonoBehaviour 
+public class PopupWindow : MonoBehaviour
 {
     private bool showMenu = false;
     private bool stopAnim = false;
 
+    public Button menuBtn;
     public GameObject menu;
+
+    private Color WHITE = new Color( 1.0f, 1.0f, 1.0f );
+    private Color DARK_GRAY = new Color( 0.5f, 0.5f, 0.5f );
    
     public void ToggleMenu ()
     {
         showMenu = !showMenu;
-        menu.SetActive( showMenu );       
+        menu.SetActive( showMenu );      
+
+        if ( showMenu )
+        {
+            ChangeButtonColor( DARK_GRAY );
+        }
+        else
+        {
+            ChangeButtonColor( WHITE );
+        }
     }	
+
+    private void ChangeButtonColor( Color color )
+    {
+        Image menuImage = menuBtn.image;
+        menuImage.color = color;
+    }
 }
